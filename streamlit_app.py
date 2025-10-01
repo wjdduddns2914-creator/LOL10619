2024adb16602b1e19e1c531082db652a6c7eacc3
wjdduddns2914-creator <wjdduddns2914@gmail.com>
2025-10-01 09:10:34 +0000

    첫 번째 앱 배포

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 정영운의 첫번째 앱!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "반갑습니다. 저는 1학년 6반 정영운입니다."
 )
+st.success("똥")
+st.info("싸")
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgLYELrfWxO0c3gzrzbSg4qGAYJedosxKSfg&s")
