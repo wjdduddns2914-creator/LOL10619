ae52c4297550e43812e7a209074963e579e9fc45
wjdduddns2914-creator <wjdduddns2914@gmail.com>
2025-10-01 09:29:45 +0000

    ㅇㅇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ st.title("배가 아프나요?")
 st.write(
     "반갑습니다. 저는 똥입니다. 지금부터 여러분의 장을 자극 하겠습니다."
 )
+
+if st.button("클릭하세요"):
+    st.write("뿌지직!")
 st.success("똥")
 st.info("싸")
 st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgLYELrfWxO0c3gzrzbSg4qGAYJedosxKSfg&s")
